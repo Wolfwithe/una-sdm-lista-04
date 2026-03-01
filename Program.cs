883c05a892227c31dab1287f5517bfa0819840bf
Eduardo <eduardoolivera.lana@gmail.com>
2026-03-01 17:51:47 -0300

    Add files via upload
    https://github.com/Wolfwithe

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+
+        string json = await client.GetStringAsync("https://api.adviceslip.com/advice");
+
+        using JsonDocument doc = JsonDocument.Parse(json);
+
+        string advice = doc.RootElement
+                           .GetProperty("slip")
+                           .GetProperty("advice")
+                           .GetString();
+
+        Console.WriteLine("Conselho do dia:");
+        Console.WriteLine(advice);
+    }
+}
